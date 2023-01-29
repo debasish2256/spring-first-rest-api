4c02598cda2134a6e252961a514e0b6ae0cfe6fc
debasish2256 <debasish.kiitecell@gmail.com>
2023-01-29 19:53:46 +0530

    enhanced with java 11

--- spring-first-rest-api/src/main/java/com/spring/project/ChuckNorrisController.java
+++ spring-first-rest-api/src/main/java/com/spring/project/ChuckNorrisController.java
@@ -1,5 +1,13 @@
 package com.spring.project;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +22,20 @@ public class ChuckNorrisController {
 		String url = "https://api.chucknorris.io/jokes/random?category="+category;
 		String joke = restTemplate.getForObject(url, String.class);
 		return joke;
+	}
+	
+	
+	@GetMapping("/joke-json")
+	public ResponseEntity<String> getJokeWithJsonResponse(@RequestParam String category) throws IOException, InterruptedException, JSONException {
+		var uri = URI.create("https://api.chucknorris.io/jokes/random?category="+category);
+		var client = HttpClient.newHttpClient();
+		var request = HttpRequest
+		        	  .newBuilder()
+		        	  .uri(uri)
+		        	  .header("accept", "application/json")
+		        	  .GET()
+		        	  .build();
+		var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		return ResponseEntity.status(response.statusCode()).body(response.body());
 	}
 }
