ee0387655bffe33bd087422b44e28cbb9af744d5
Debasish Mahana <debasishmahana49@gmail.com>
2023-04-12 04:07:56 +0530

    Nationalize
    Nationalize

---
+++ spring-first-rest-api/src/main/java/com/spring/project/service/Nationalize.java
@@ -0,0 +1,25 @@
+package com.spring.project.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Nationalize {
+	public static void main(String[] args) throws Exception {
+		URL url = new URL("https://api.nationalize.io/?name=nathaniel");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+        in.close();
+        con.disconnect();
+        String jsonData = content.toString();
+        System.out.println(jsonData);
+    }
+}
